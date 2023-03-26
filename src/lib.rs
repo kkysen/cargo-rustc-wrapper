59b54464147b3d910c081e790fadd89a04c18070
Khyber Sen <kkysen@gmail.com>
2023-03-26 02:54:31 -0400

    Add `EnvVar::{get, get_os, get_path}` and hardcoded `K = &'static str`.

--- src/lib.rs
+++ src/lib.rs
@@ -6,32 +6,59 @@ use std::process;
 use std::process::Command;
 use std::process::ExitStatus;
 
-struct EnvVar<K, V>
+#[derive(PartialEq, Eq)]
+struct EnvVar<V>
 where
-    K: AsRef<OsStr>,
     V: AsRef<OsStr>,
 {
-    key: K,
-    value: V,
+    pub key: &'static str,
+    pub value: V,
 }
 
-impl<K, V> EnvVar<K, V>
+impl<V> EnvVar<V>
 where
-    K: AsRef<OsStr>,
     V: AsRef<OsStr>,
 {
     pub fn set_on(&self, cmd: &mut Command) {
-        cmd.env(self.key.as_ref(), self.value.as_ref());
+        cmd.env(self.key, self.value.as_ref());
     }
 
     pub fn set(&self) {
-        env::set_var(self.key.as_ref(), self.value.as_ref());
+        env::set_var(self.key, self.value.as_ref());
     }
 }
 
-type RustcWrapperEnvVar = EnvVar<&'static str, PathBuf>;
-type SysrootEnvVar = EnvVar<&'static str, PathBuf>;
-type ToolchainEnvVar = EnvVar<&'static str, String>;
+impl EnvVar<OsString> {
+    pub fn get_os(key: &'static str) -> Option<Self> {
+        Some(Self {
+            key,
+            value: env::var_os(key)?,
+        })
+    }
+}
+
+impl EnvVar<String> {
+    pub fn get(key: &'static str) -> Result<Self, env::VarError> {
+        Ok(Self {
+            key,
+            value: env::var(key)?,
+        })
+    }
+}
+
+impl EnvVar<PathBuf> {
+    pub fn get_path(key: &'static str) -> Option<Self> {
+        let EnvVar { key, value } = EnvVar::get_os(key)?;
+        Some(Self {
+            key,
+            value: PathBuf::from(value),
+        })
+    }
+}
+
+type RustcWrapperEnvVar = EnvVar<PathBuf>;
+type SysrootEnvVar = EnvVar<PathBuf>;
+type ToolchainEnvVar = EnvVar<String>;
 
 fn exit_with_status(status: ExitStatus) {
     process::exit(status.code().unwrap_or(1))
