96142d68fc6a6b7d1cba07e3dac78dc66199c981
Khyber Sen <kkysen@gmail.com>
2023-03-26 01:33:03 -0400

    Add `cargo_rustc_wrapper` w/ impl and `{cargo,rustc}_wrapper` w/ `todo!()`s.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{env, path::Path};
+
+const RUSTC_WRAPPER_VAR: &str = "RUSTC_WRAPPER";
+
+/// Run as a `rustc` wrapper (a la `$RUSTC_WRAPPER`/[`RUSTC_WRAPPER_VAR`]).
+fn rustc_wrapper() -> anyhow::Result<()> {
+    todo!()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Run as a `cargo` wrapper/plugin, the default invocation.
+fn cargo_wrapper(_rustc_wrapper: &Path) -> anyhow::Result<()> {
+    todo!()
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// Run the current binary as either a `cargo` or `rustc` wrapper.
+pub fn cargo_rustc_wrapper() -> anyhow::Result<()> {
+    let own_exe = env::current_exe()?;
+
+    let wrapping_rustc = env::var_os(RUSTC_WRAPPER_VAR).as_deref() == Some(own_exe.as_os_str());
+    if wrapping_rustc {
+        rustc_wrapper()
+    } else {
+        cargo_wrapper(&own_exe)
     }
 }
